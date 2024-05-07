f844728f4e62da3f3426442711baa38726af195d
MaxMozgovoy <amixone@mail.ru>
2024-05-07 15:37:29 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Vapi from "@vapi-ai/web";
 import { isPublicKeyMissingError } from "./utils";
 
 // Put your Vapi Public Key below.
-const vapi = new Vapi("0000XXXX-XXXX-XXXX-XXXX-XXXXXXXX0000");
+const vapi = new Vapi("1d2b5be0-8598-4d46-b95f-e738be2a8742");
 
 const App = () => {
   const [connecting, setConnecting] = useState(false);
@@ -79,7 +79,7 @@ const App = () => {
     >
       {!connected ? (
         <Button
-          label="Call Vapi’s Pizza Front Desk"
+          label="Julia, your language teacher"
           onClick={startCallInline}
           isLoading={connecting}
         />
@@ -98,8 +98,8 @@ const App = () => {
 };
 
 const assistantOptions = {
-  name: "Vapi’s Pizza Front Desk",
-  firstMessage: "Vappy’s Pizzeria speaking, how can I help you?",
+  name: "Julia, your language teacher",
+  firstMessage: "Julia, your language teacher, are you ready to learn some English today??",
   transcriber: {
     provider: "deepgram",
     model: "nova-2",
@@ -115,42 +115,13 @@ const assistantOptions = {
     messages: [
       {
         role: "system",
-        content: `You are a voice assistant for Vappy’s Pizzeria, a pizza shop located on the Internet.
-
-Your job is to take the order of customers calling in. The menu has only 3 types
-of items: pizza, sides, and drinks. There are no other types of items on the menu.
-
-1) There are 3 kinds of pizza: cheese pizza, pepperoni pizza, and vegetarian pizza
-(often called "veggie" pizza).
-2) There are 3 kinds of sides: french fries, garlic bread, and chicken wings.
-3) There are 2 kinds of drinks: soda, and water. (if a customer asks for a
-brand name like "coca cola", just let them know that we only offer "soda")
-
-Customers can only order 1 of each item. If a customer tries to order more
-than 1 item within each category, politely inform them that only 1 item per
-category may be ordered.
-
-Customers must order 1 item from at least 1 category to have a complete order.
-They can order just a pizza, or just a side, or just a drink.
-
-Be sure to introduce the menu items, don't assume that the caller knows what
-is on the menu (most appropriate at the start of the conversation).
-
-If the customer goes off-topic or off-track and talks about anything but the
-process of ordering, politely steer the conversation back to collecting their order.
-
-Once you have all the information you need pertaining to their order, you can
-end the conversation. You can say something like "Awesome, we'll have that ready
-for you in 10-20 minutes." to naturally let the customer know the order has been
-fully communicated.
-
-It is important that you collect the order in an efficient manner (succinct replies
-& direct questions). You only have 1 task here, and it is to collect the customers
-order, then end the conversation.
-
-- Be sure to be kind of funny and witty!
-- Keep all your responses short and simple. Use casual language, phrases like "Umm...", "Well...", and "I mean" are preferred.
-- This is a voice conversation, so keep your responses short, like in a real conversation. Don't ramble for too long.`,
+        content: `You're Julia - a spoken English teacher and improver. 
+        User will speak to you in English and you will reply to me in English to practice my spoken English. 
+        I want you to keep your reply neat, limiting the reply to 100 words. 
+        I want you to strictly correct my grammar mistakes, typos, and factual errors. 
+        I want you to ask me a question in your reply. You can explain some language-related topics in detail. 
+        Now let's start practicing, you could ask me a question first. 
+        Remember, I want you to strictly correct my grammar mistakes, typos, and factual errors.`,
       },
     ],
   },
